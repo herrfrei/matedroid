ab45b61d260056892da592eb7e72ab03f5e32159
Miguel Ángel <marm.developer@gmail.com>
2026-02-12 21:15:10 +0100

    Format Int to formated number
    Show thousand separator when int number is greather han 999

--- app/src/main/java/com/matedroid/ui/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/matedroid/ui/screens/stats/StatsScreen.kt
@@ -603,12 +603,12 @@ private fun QuickStatsDrivesCard(quickStats: QuickStats, palette: CarColorPalett
         Row(modifier = Modifier.fillMaxWidth()) {
             StatItem(
                 label = stringResource(R.string.stats_total_drives),
-                value = quickStats.totalDrives.toString(),
+                value = "%,d".format(quickStats.totalDrives),
                 modifier = Modifier.weight(1f)
             )
             StatItem(
                 label = stringResource(R.string.stats_driving_days),
-                value = quickStats.totalDrivingDays?.toString() ?: "-",
+                value = quickStats.totalDrivingDays?.let { "%,d".format(it) } ?: "-",
                 modifier = Modifier.weight(1f)
             )
         }
@@ -651,7 +651,7 @@ private fun QuickStatsChargesCard(quickStats: QuickStats, palette: CarColorPalet
         Row(modifier = Modifier.fillMaxWidth()) {
             StatItem(
                 label = stringResource(R.string.stats_total_charges),
-                value = quickStats.totalCharges.toString(),
+                value = "%,d".format(quickStats.totalCharges),
                 modifier = Modifier.weight(1f)
             )
             StatItem(
